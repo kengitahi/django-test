35c90d041788e0e25e6f88fb9daf0562fb9c2cbd
Kennedy Gitahi <kengitahi@hotmail.com>
2025-07-07 20:11:41 +0300

    Change base admin page

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Movie, Show
+
+
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("name", "release_year", "imdb_rating")
+    search_fields = ("name", "release_year")
+
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ("name", "release_date", "imdb_rating")
+    search_fields = ("name", "release_date")
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Show, ShowAdmin)
